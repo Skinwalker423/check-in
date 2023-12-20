048c6f7c6f88026879526727055154f8e2c74722
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-19 21:46:42 -0800

    Row

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,43 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import styled from "styled-components";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Heading as='h1'>Hello World</Heading>
-      <Heading as='h2'>Hello World</Heading>
-      <Heading as='h3'>Hello World</Heading>
-      <Input
-        type='text'
-        placeholder='Enter username'
-        id='name'
-        name='name'
-      />
-      <Button>Submit</Button>
+      <StyledApp>
+        <Row type='vertical'>
+          <Row type='horizontal'>
+            <Heading as='h1'>Hello World</Heading>
+
+            <div>
+              <Input
+                type='text'
+                placeholder='Enter username'
+                id='name'
+                name='name'
+              />
+              <Button>Submit</Button>
+            </div>
+          </Row>
+          <Row type='vertical'>
+            <Heading as='h3'>Form</Heading>
+            <form action=''>
+              <Input
+                placeholder='Number of guests'
+                type='number'
+              />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
   );
 }

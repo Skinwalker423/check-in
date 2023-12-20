ba1b10871e3d9059e8b80091202f880fba1b7b3f
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-19 21:29:07 -0800

    Heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  color: blue;
-`;
+import Heading from "./ui/Heading";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <H1>Hello World</H1>
+      <Heading as='h1'>Hello World</Heading>
+      <Heading as='h2'>Hello World</Heading>
+      <Heading as='h3'>Hello World</Heading>
       <Input
         type='text'
         placeholder='Enter username'

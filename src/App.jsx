8aa392407128f362e7fa756826a3ec6f44bcf0d8
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-20 21:54:22 -0800

    app layout

--- src/App.jsx
+++ src/App.jsx
@@ -12,25 +12,39 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import NewUsers from "./pages/Users";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          element={<Navigate replace to={"dashboard"} />}
-          index
-        />
-        <Route path='dashboard' element={<Dashboard />} />
-        <Route path='cabins' element={<Cabins />} />
-        <Route path='bookings' element={<Bookings />} />
-        <Route path='account' element={<Account />} />
-        <Route path='login' element={<Login />} />
-        <Route path='settings' element={<Settings />} />
-        <Route path='users' element={<NewUsers />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route
+              element={
+                <Navigate replace to={"dashboard"} />
+              }
+              index
+            />
+            <Route
+              path='dashboard'
+              element={<Dashboard />}
+            />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='account' element={<Account />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='users' element={<NewUsers />} />
+          </Route>
+
+          <Route path='login' element={<Login />} />
+
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 

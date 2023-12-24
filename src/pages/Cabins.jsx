f25d369952c0d58acbfe29d68fa8065388aa942f
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-23 21:15:16 -0800

    fetching cabins in cabin page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [cabins, setCabins] = useState();
+
+  useEffect(() => {
+    getCabins().then((data) => {
+      if (data) setCabins(data);
+    });
+  }, []);
+
+  console.log("cabins", cabins);
+
   return (
-    <Row type='horizontal'>
+    <Row type='vertical'>
       <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
+      <ul>
+        {cabins.length > 0 &&
+          cabins.map((cabin) => {
+            return (
+              <li key={cabin.id}>
+                <p>Room Number: {cabin.name}</p>
+                <p>{cabin.description}</p>
+              </li>
+            );
+          })}
+      </ul>
     </Row>
   );
 }

0500a14fb1ffe157c01806e5bd12a0f3a53191ab
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-23 20:59:02 -0800

    getCabins

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+export async function getCabins() {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("problem getting cabins");
+  }
+
+  return data;
+}

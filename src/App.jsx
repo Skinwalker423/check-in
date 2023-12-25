2d6d1b501aad6ca89a5e9af5e86290910a43d7f4
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-24 22:37:56 -0800

    tanstack dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -31,6 +32,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>

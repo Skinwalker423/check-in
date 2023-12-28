ae07c93f19dbb56d314f4b02197883bdae25143a
Luis Gonzalez <lgonzalez23@verizon.net>
2023-12-27 18:13:28 -0800

    create cabin form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <Row type='vertical'>
       <Heading as='h1'>All cabins</Heading>
       <CabinTable />
+      <Button
+        type='button'
+        onClick={() => setShowForm((prev) => !prev)}
+      >
+        {showForm ? "Hide Form" : "Add New Cabin"}
+      </Button>
+      {showForm && <CreateCabinForm />}
     </Row>
   );
 }

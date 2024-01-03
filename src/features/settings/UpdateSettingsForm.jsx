8f9b7485af0641449c7d11d464b67f33fecbd898
Luis Gonzalez <lgonzalez23@verizon.net>
2024-01-02 20:36:07 -0800

    useform

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,61 +1,96 @@
 import { useForm } from "react-hook-form";
+
+import { useSettings } from "./useSettings";
+
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-import { useSettings } from "./useSettings";
+import Spinner from "../../ui/Spinner";
 
 function UpdateSettingsForm() {
   const { error, isLoading, settings } = useSettings();
 
   console.log("settings", settings, error, isLoading);
 
-  const { getValues, register } = useForm({
-    defaultValues: {
-      ...settings,
-    },
-  });
+  const { getValues, register, handleSubmit, formState } =
+    useForm({
+      defaultValues: {
+        ...settings,
+      },
+    });
+
+  const { errors } = formState;
   const values = getValues();
   console.log("values", values);
 
+  const onSubmit = () => {
+    console.log("submitted");
+  };
+
+  const onError = (errors) => {
+    console.log(errors);
+  };
+
+  console.log("errors", errors);
+
+  if (isLoading) return <Spinner />;
+
   return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow
+        label='Minimum nights/booking'
+        error={errors?.minBookingLength?.message}
+      >
         <Input
           type='number'
           id='minBookingLength
           '
+          defaultValue={settings?.minBookingLength}
           {...register("minBookingLength", {
             required: "enter min nights",
           })}
         />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
+      <FormRow
+        label='Maximum nights/booking'
+        error={errors?.maxBookingLength?.message}
+      >
         <Input
           type='number'
           id='maxBookingLength'
+          defaultValue={settings?.maxBookingLength}
           {...register("maxBookingLength", {
             required: "enter max nights",
           })}
         />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
+      <FormRow
+        label='Maximum guests/booking'
+        error={errors?.maxGuestsPerBooking?.message}
+      >
         <Input
           type='number'
           id='maxGuestsPerBooking'
+          defaultValue={settings?.maxBookingLength}
           {...register("maxGuestsPerBooking", {
-            required: "enter min nights",
+            required: "enter max guests",
           })}
         />
       </FormRow>
-      <FormRow label='Breakfast price'>
+      <FormRow
+        label='Breakfast price'
+        error={errors?.breakfastPrice?.message}
+      >
         <Input
           type='number'
           id='breakfastPrice'
+          defaultValue={settings?.breakfastPrice}
           {...register("breakfastPrice", {
-            required: "enter min nights",
+            required: "enter breakfast price",
           })}
         />
       </FormRow>
+      <button type='submit'>Update Settings</button>
     </Form>
   );
 }

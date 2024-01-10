ee09f12201ef272229ff3da719874541ace7e04b
Luis Gonzalez <lgonzalez23@verizon.net>
2024-01-09 21:03:48 -0800

    stylesheetmanager

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { StyleSheetManager } from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
 import App from "./App.jsx";
+
+function shouldForwardProp(propName, target) {
+  if (typeof target === "string") {
+    // For HTML elements, forward the prop if it is a valid HTML attribute
+    return isPropValid(propName);
+  }
+  // For other elements, forward all props
+  return true;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <StyleSheetManager
+      shouldForwardProp={shouldForwardProp}
+    >
+      <App />
+    </StyleSheetManager>
   </React.StrictMode>
 );

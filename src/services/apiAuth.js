1a63f4304fead870e9f1700ff0ef9808f9b48a6f
Luis Gonzalez <lgonzalez23@verizon.net>
2024-01-25 12:53:08 -0800

    signup

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -46,3 +46,16 @@ export async function getSession() {
 
   return user;
 }
+
+export const signup = async ({ email, password }) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+};

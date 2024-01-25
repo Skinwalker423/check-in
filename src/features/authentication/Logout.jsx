169a9dc4ef45ba976c2a29cd458fd4f83c836864
Luis Gonzalez <lgonzalez23@verizon.net>
2024-01-25 09:22:04 -0800

    Logout component

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,12 @@
+import ButtonIcon from "../../ui/ButtonIcon";
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+
+const Logout = () => {
+  return (
+    <ButtonIcon>
+      <HiArrowRightOnRectangle />
+    </ButtonIcon>
+  );
+};
+
+export default Logout;

8e0945baa9ec924c2f843fc4b657a09f690b15cf
Luis Gonzalez <lgonzalez23@verizon.net>
2024-01-26 11:55:18 -0800

    useUpdateUser

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -1,5 +1,42 @@
+import { useMutation } from "@tanstack/react-query";
+import { updateUser as updateUserApi } from "../../services/apiAccount";
+import toast from "react-hot-toast";
+
 const useUpdateUser = () => {
-  return <div>useUpdateUser</div>;
+  const {
+    mutate: updateUser,
+    error,
+    isLoading,
+  } = useMutation({
+    mutationFn: ({
+      avatar,
+      fullName,
+      password,
+      currentAvatar,
+    }) => {
+      updateUserApi({
+        avatar,
+        fullName,
+        password,
+        currentAvatar,
+      });
+    },
+    mutationKey: ["user"],
+    onSuccess: (data) => {
+      console.log("data", data);
+      toast.success("successfully updated profile");
+    },
+    onError: (error) => {
+      toast.error(error.message, {
+        duration: 7000,
+        style: {
+          backgroundColor: "red",
+          color: "white",
+        },
+      });
+    },
+  });
+  return { updateUser, isLoading, error };
 };
 
 export default useUpdateUser;
